95e68bca683a7c3039bf60ad7f776890638b6cb0
Jouni Leino <jouni.ilari.leino@gmail.com>
2017-08-18 13:59:20 +0200

    Fetching values from json object

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const request = require('request');
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadephia',
-  jeon: true
+  json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
